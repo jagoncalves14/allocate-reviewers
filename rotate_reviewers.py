1baab3057af51a31a3412e53813f542f9b4e0356
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-03-27 05:30:00 +0300

    Add logic to support preferring reviewers

--- rotate_reviewers.py
+++ rotate_reviewers.py
@@ -1,3 +1,4 @@
+import math
 import traceback
 from typing import List
 
@@ -26,10 +27,26 @@ def get_previous_allocation() -> dict[str, str]:
     if not previous_allocation_:
         return {}
 
+    if len(developer_names) != len(previous_allocation_):
+        return {}
+
     developer_names.pop(0)
     previous_allocation_.pop(0)
     result = dict(zip(developer_names, previous_allocation_))
     return result
+
+
+def add_preferable_reviewers(devs: List[Developer]) -> None:
+    for dev in devs:
+        if not dev.preferable_reviewer_names:
+            continue
+
+        for preferable_name in dev.preferable_reviewer_names:
+            preferable_reviewer = next(
+                dev_ for dev_ in devs if dev_.name == preferable_name
+            )
+            dev.reviewer_names.add(preferable_name)
+            preferable_reviewer.review_for.add(dev.name)
 
 
 def rotate_reviewers(devs: List[Developer], previous_allocation_: dict) -> None:
@@ -37,6 +54,10 @@ def rotate_reviewers(devs: List[Developer], previous_allocation_: dict) -> None:
     Assign reviewers to input developers.
     The function mutate directly the input argument "devs".
     """
+    maximum_assignment = math.ceil(
+        sum(dev_.reviewer_number for dev_ in devs) // len(devs)
+    )
+
     devs.sort(key=lambda dev_: dev_.order, reverse=False)
     previous_reviewer_names_of_first_dev = previous_allocation_.get(
         devs[0].name, ""
@@ -55,16 +76,30 @@ def rotate_reviewers(devs: List[Developer], previous_allocation_: dict) -> None:
         else 0
     )
     for dev in devs:
-        overlap = 0
-        for review_number in range(1, dev.reviewer_number + 1):
-            index = starting_index + review_number + overlap
-            safe_index = index % len(devs)
-            # Maximum 1 overlap (developer's name == reviewer's name) per dev per run.
-            if dev.name == devs[safe_index].name:
-                overlap += 1
-                index = starting_index + review_number + overlap
-                safe_index = index % len(devs)
+        skip = 0
+        remaining_reviewer_number = min(
+            0, dev.reviewer_number - len(dev.reviewer_names)
+        )
+        for review_number in range(1, remaining_reviewer_number + 1):
+            current_skip = skip
 
+            def get_safe_numbers(skip_):
+                index_ = starting_index + review_number + skip_
+                safe_index_ = index_ % len(devs)
+                if (
+                    devs[safe_index_].name == dev.name
+                    or devs[safe_index_].name in dev.reviewer_names
+                ):
+                    return get_safe_numbers(starting_index, review_number, skip_ + 1)
+
+                if len(
+                    devs[safe_index_].review_for
+                ) >= maximum_assignment and skip_ - current_skip < len(devs):
+                    return get_safe_numbers(starting_index, review_number, skip_ + 1)
+
+                return safe_index_, index_, skip_
+
+            safe_index, index, skip = get_safe_numbers(skip)
             reviewer = devs[safe_index]
             dev.reviewer_names.add(reviewer.name)
             reviewer.review_for.add(dev.name)
@@ -76,6 +111,7 @@ if __name__ == "__main__":
     try:
         developers = load_developers_from_sheet()
         arrange_developers(developers)
+        add_preferable_reviewers(developers)
         previous_allocation = get_previous_allocation()
         rotate_reviewers(developers, previous_allocation)
         write_reviewers_to_sheet(developers)
