84b089d13d4cade7aa7b18381d15bf0d9cb69ebf
João Gonçalves <joao.goncalves@nordhealth.com>
2025-10-22 21:24:56 +0200

    feat: update column constants

--- env_constants.py
+++ env_constants.py
@@ -10,25 +10,39 @@ DRIVE_SCOPE = [
 ]
 SHEET_NAME = os.environ.get("SHEET_NAME")
 
-DEVELOPER_HEADER = "Developer"
-TEAM_DEVELOPER_HEADER = "Team Developers"
-REVIEWER_NUMBER_HEADER = "Number of Reviewers"
-PREFERABLE_REVIEWER_HEADER = "Preferable Reviewers"
-ALLOCATION_INDEXES_HEADER = "Indexes"
-
-DEFAULT_REVIEWER_NUMBER = int(os.environ.get("DEFAULT_REVIEWER_NUMBER") or "1")
-EXPERIENCED_DEV_NAMES = set(os.environ.get("EXPERIENCED_DEV_NAMES", "").split(", "))
-
-EXPECTED_HEADERS_FOR_ALLOCATION = [
-    DEVELOPER_HEADER,
-    REVIEWER_NUMBER_HEADER,
-    PREFERABLE_REVIEWER_HEADER,
+# Column names for FE Devs allocation
+FE_DEVS_COLUMNS = [
+    "Developer",  # Column 0: Developer name
+    "Number of Reviewers",  # Column 1: Number of reviewers
+    "Preferable Reviewers",  # Column 2: Preferred reviewer names
 ]
 
-
-EXPECTED_HEADERS_FOR_ROTATION = [
-    TEAM_DEVELOPER_HEADER,
-    REVIEWER_NUMBER_HEADER,
-    ALLOCATION_INDEXES_HEADER,
+# Column names for Teams rotation
+TEAMS_COLUMNS = [
+    "Team Developers",  # Column 0: Team/Developer name
+    "Number of Reviewers",  # Column 1: Number of reviewers
+    "Indexes",  # Column 2: Rotation indexes
 ]
-REVIEWERS_CONFIG_LIST = list(os.environ.get("REVIEWERS_CONFIG_LIST", "").split(", "))
+
+# Convenient access to column names by index
+DEVELOPER_HEADER = FE_DEVS_COLUMNS[0]
+REVIEWER_NUMBER_HEADER = FE_DEVS_COLUMNS[1]
+PREFERABLE_REVIEWER_HEADER = FE_DEVS_COLUMNS[2]
+
+TEAM_DEVELOPER_HEADER = TEAMS_COLUMNS[0]
+ALLOCATION_INDEXES_HEADER = TEAMS_COLUMNS[2]
+
+# Expected headers for each sheet type
+EXPECTED_HEADERS_FOR_ALLOCATION = FE_DEVS_COLUMNS
+EXPECTED_HEADERS_FOR_ROTATION = TEAMS_COLUMNS
+
+# Environment variables
+DEFAULT_REVIEWER_NUMBER = int(
+    os.environ.get("DEFAULT_REVIEWER_NUMBER") or "1"
+)
+EXPERIENCED_DEV_NAMES = set(
+    os.environ.get("EXPERIENCED_DEV_NAMES", "").split(", ")
+)
+REVIEWERS_CONFIG_LIST = list(
+    os.environ.get("REVIEWERS_CONFIG_LIST", "").split(", ")
+)
